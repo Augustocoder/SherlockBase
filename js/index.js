2d7222d33f4f9cd2eb9463aeb9c349f1de36be58
Pedro Borges <zpedrogod59@gmail.com>
2023-04-30 10:52:28 -0300

    Adicionando Design novo, corrigindo erros na consulta e adicionando melhorias.

--- js/index.js
+++ js/index.js
@@ -1,21 +1,24 @@
-$(document).ready(function() {
-    $("#buscar").submit(function(event) {
-        event.preventDefault();
-        var cpf = $("input[name='cpf_cnpj']").val();
-        $("#carregando").show();
-        $.ajax({
-            type: "GET",
-            url: "consultar.php?cpf_cnpj=" + cpf,
-            dataType: "text",
-        })
-        .done(function(msg) {
-            $("#carregando").hide();
-            $("#result").html('<h2>Resultado:</h2>');
-            $("#result").html(msg);
-        })
-        .fail(function(jqXHR, textStatus, msg) {
-            $("#carregando").hide();
-            alert(msg);
-        });
-    });
+$(document).ready(function () {
+  $("#buscar").submit(function (event) {
+    event.preventDefault();
+    var cpf = $("input[name='cpf_cnpj']").val();
+    $("#carregando").show();
+    $("#icon").hide();
+
+    $.ajax({
+      type: "GET",
+      url: "consultar.php?cpf_cnpj=" + cpf,
+      dataType: "text",
+    })
+      .done(function (msg) {
+        $("#carregando").hide();
+        $("#icon").show();
+        $("#info").hide();
+        $("#result").html(msg);
+      })
+      .fail(function (jqXHR, textStatus, msg) {
+        $("#carregando").hide();
+        alert(msg);
+      });
   });
+});
